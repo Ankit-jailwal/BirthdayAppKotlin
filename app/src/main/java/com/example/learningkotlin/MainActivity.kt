bfd6fb406490c3c3b4ea3b586880d471903fb689
Ankit Jailwal <jailwalankit@gmail.com>
2022-04-23 01:11:08 +0530

    Learned about TextView and EditText

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.learningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = nameInput.editableText.toString()
+        Toast.makeText(this, "Happy Birthday $name", Toast.LENGTH_SHORT).show()
     }
 }
